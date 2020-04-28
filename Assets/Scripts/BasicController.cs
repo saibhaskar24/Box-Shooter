3f8d829eb18f2da0df5a0da198e3738a3338aba5
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-04-28 15:38:46 +0530

    final

---
+++ Assets/Scripts/BasicController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public float gravity = 9.81f;
+
+    public CharacterController myController;
+
+    void Start()
+    {
+        myController = gameObject.GetComponent<CharacterController>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementZ = Input.GetAxis("Vertical") * Vector3.forward * moveSpeed * Time.deltaTime;
+        Vector3 movementX = Input.GetAxis("Horizontal" ) * Vector3.right * moveSpeed * Time.deltaTime;
+
+        Vector3 movement = transform.TransformDirection(movementX + movementZ);
+
+        movement.y -= gravity * Time.deltaTime;
+
+        Debug.Log("Movement "+ movement + "movement Z" + movementZ);
+
+        myController.Move(movement);
+
+    }
+}
---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public enum MotionDirections { Horizontal , Spin , Vetical };
+
+    public MotionDirections motionState = MotionDirections.Horizontal; 
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(motionState)
+        {
+            case MotionDirections.Spin:
+                gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+                break;
+            case MotionDirections.Horizontal:
+                gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+            case MotionDirections.Vetical:
+                gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+        }
+    }
+}
